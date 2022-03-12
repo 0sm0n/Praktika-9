93aeb78e7a63eb3268436f633926c2f5fd4b22bf
is42-shekerbekuuluo <shekerbekuuluosmon@gmail.com>
2022-03-12 11:40:36 +0500

    Praktika-9

---
+++ work_1.php
@@ -0,0 +1,14 @@
+<?php
+function test($i, $w)
+{
+    return area($i, $w);
+
+}
+function area($i, $w)
+{
+    $area = $i*$w;
+    return ("Прямоугольник длиной $i и шириной $w имеет площадь $area");
+
+}
+
+echo test(5, 4);
